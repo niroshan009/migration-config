bf0f2a9e3642ad094aa0d9faab039da3ef629e5c
thusitha.k <tkularathna@mitrai.com>
2019-10-01 12:00:21 +0530

    wiremock hook for support

---
+++ cucumber/src/test/java/com/niroshan/support/WireMockHook.java
@@ -0,0 +1,32 @@
+package com.niroshan.support;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+
+public class WireMockHook {
+
+	private int wireMockPort = 9110;
+	private String wireMockHost = "localhost";
+	private WireMockServer wireMockServer;
+	private WireMock wireMock;
+
+	@Before("@wiremockApi")
+	public void setupWiremockServer() throws InterruptedException {
+		wireMockServer = new WireMockServer(wireMockPort);
+		wireMock = new WireMock(wireMockHost, wireMockPort);
+		wireMockServer.start();
+		
+		wireMock.register(WireMock.get(WireMock.urlPathMatching("/testGet")).willReturn(aResponse().withStatus(200)
+				.withHeader("Content-Type", "application/json").withBody("{\"message\": \"Success\"}")));
+		Thread.sleep(50000);
+	}
+	
+	@After("@wiremockApi")
+	public void teardownWiremockServer() {
+		wireMockServer.stop();
+	}
+}
